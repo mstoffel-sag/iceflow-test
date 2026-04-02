f8e4eba5153b12ec8fffd6f84bf0414c42de091a
Marco Stoffel <marco.stoffel@cumulocity.com>
2026-04-02 12:49:37 +0200

    Update readme

--- spark/create_views.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW default.cdc__measurement__c8y_Temperature AS SELECT * FROM nessie.`default`.`t2027824580`.`cdc`.`measurement`.`c8y_Temperature`;
-CREATE OR REPLACE VIEW default.cdc__event__event AS SELECT * FROM nessie.`default`.`t2027824580`.`cdc`.`event`.`event`;
-CREATE OR REPLACE VIEW default.cdc__event__c8y_Position AS SELECT * FROM nessie.`default`.`t2027824580`.`cdc`.`event`.`c8y_Position`;
-CREATE OR REPLACE VIEW default.cdc__event__jobStatus AS SELECT * FROM nessie.`default`.`t2027824580`.`cdc`.`event`.`jobStatus`;
-CREATE OR REPLACE VIEW default.cdc__alarm__alarm AS SELECT * FROM nessie.`default`.`t2027824580`.`cdc`.`alarm`.`alarm`;
-CREATE OR REPLACE VIEW default.cdc__inventory__inventory AS SELECT * FROM nessie.`default`.`t2027824580`.`cdc`.`inventory`.`inventory`;
-CREATE OR REPLACE VIEW default.cdc__inventory__c8y_Position AS SELECT * FROM nessie.`default`.`t2027824580`.`cdc`.`inventory`.`c8y_Position`;
